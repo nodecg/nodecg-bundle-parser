30d0b5c03c3cb36b0cff370916860735a9b13152
Alex Van Camp <email@alexvan.camp>
2015-07-30 10:03:22 -0500

    Increase strictness, throw more errors, add more tests

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -12,7 +12,7 @@ describe('bundle parser', function () {
     });
 
     it('should return the expected data when nodecg.json does exist', function () {
-        var parsedBundle = parseBundle('./test/test-bundle');
+        var parsedBundle = parseBundle('./test/test_bundles/good-bundle');
         parsedBundle.name.should.equal('test-bundle');
         parsedBundle.version.should.equal('0.0.1');
         parsedBundle.description.should.equal('A test bundle');
@@ -36,9 +36,76 @@ describe('bundle parser', function () {
     });
 
     context('when bundleCfgPath is provided', function() {
-        it('should parse the config and add it as bundle.config', function() {
-            var parsedBundle = parseBundle('./test/test-bundle', './test/test-bundle/bundleConfig.json');
-            parsedBundle.config.should.deep.equal({foo: 'foo'});
+        context('and the file exists', function() {
+            it('should parse the config and add it as bundle.config', function() {
+                var parsedBundle = parseBundle('./test/test_bundles/good-bundle',
+                    './test/test_bundles/good-bundle/bundleConfig.json');
+                parsedBundle.config.should.deep.equal({foo: 'foo'});
+            });
+        });
+
+        context('and the file does not exist', function() {
+            it('should throw an error', function() {
+                expect(
+                    parseBundle.bind(parseBundle, './test/test_bundles/good-bundle', './made/up/path.json')
+                ).to.throw(/does not exist/);
+            });
+        });
+
+        context('and the file isn\'t valid JSON', function() {
+            it('should throw an error', function() {
+                expect(
+                    parseBundle.bind(parseBundle, './test/test_bundles/bad-json',
+                        './test/test_bundles/bad-json/bundleConfig.json')
+                ).to.throw(/Ensure that it is valid JSON/);
+            });
+        });
+    });
+
+    context('when there is no "dashboard" folder', function() {
+        it('should assign an empty array to bundle.dashboard.panels', function() {
+            var parsedBundle = parseBundle('./test/test_bundles/no-dashboard-folder');
+            parsedBundle.dashboard.panels.should.be.an.instanceof(Array).and.be.empty;
+        });
+    });
+
+    context('when there is a "dashboard" folder but no "panels.json"', function() {
+        it('should throw an error', function() {
+            expect(
+                parseBundle.bind(parseBundle, './test/test_bundles/no-panelsjson')
+            ).to.throw(/"dashboard\/panels.json" was not found/);
+        });
+    });
+
+    context('when critical properties are missing from "panels.json"', function() {
+        it('should throw an error explaining what is missing', function() {
+            expect(
+                parseBundle.bind(parseBundle, './test/test_bundles/missing-panel-props')
+            ).to.throw(/the following properties: name, title, file/);
+        });
+    });
+
+    context('when two panels have the same name', function() {
+        it('should throw an error', function() {
+            expect(
+                parseBundle.bind(parseBundle, './test/test_bundles/dupe-panel-name')
+            ).to.throw(/has the same name as another panel/);
+        });
+    });
+
+    context('when a panel\'s file has no <head> tag', function() {
+        it('should throw an error', function() {
+            expect(
+                parseBundle.bind(parseBundle, './test/test_bundles/no-head')
+            ).to.throw(/has no <head>/);
+        });
+    });
+
+    context('when a panel\'s file has no <!DOCTYPE>', function() {
+        it('should throw an error', function() {
+            expect(
+                parseBundle.bind(parseBundle, './test/test_bundles/no-doctype')
+            ).to.throw(/has no DOCTYPE/);
         });
     });
 });
