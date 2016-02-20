32a901faad78bda373c148041b154d7ac8a8a009
Alex Van Camp <email@alexvan.camp>
2016-02-20 17:59:02 -0600

    style(all): adopt XO style

--- index.js
+++ index.js
@@ -9,37 +9,37 @@ var parseConfig = require('./lib/config');
 var parseExtension = require('./lib/extension');
 
 module.exports = function (bundlePath, bundleCfgPath) {
-    // Resolve the path to the bundle and its package.json
-    var manifestPath = path.join(bundlePath, 'package.json');
+	// Resolve the path to the bundle and its package.json
+	var manifestPath = path.join(bundlePath, 'package.json');
 
-    if (!fs.existsSync(manifestPath)) {
-        throw new Error('Bundle at path '+ bundlePath +' does not contain a package.json!');
-    }
+	if (!fs.existsSync(manifestPath)) {
+		throw new Error('Bundle at path ' + bundlePath + ' does not contain a package.json!');
+	}
 
-    // Read metadata from the package.json
-    var manifest = parseManifest(manifestPath);
-    var bundle = manifest;
-    bundle.rawManifest = JSON.stringify(manifest);
-    bundle.dir = bundlePath;
+	// Read metadata from the package.json
+	var manifest = parseManifest(manifestPath);
+	var bundle = manifest;
+	bundle.rawManifest = JSON.stringify(manifest);
+	bundle.dir = bundlePath;
 
-    // If there is a config file for this bundle, parse it
-    if (bundleCfgPath) {
-        bundle.config = parseConfig(bundle, bundleCfgPath);
-    }
+	// If there is a config file for this bundle, parse it
+	if (bundleCfgPath) {
+		bundle.config = parseConfig(bundle, bundleCfgPath);
+	}
 
-    // Parse the dashboard panels
-    var dashboardDir = path.resolve(bundle.dir, 'dashboard');
-    bundle.dashboard = {
-        dir: dashboardDir,
-        panels: parsePanels(dashboardDir, manifest)
-    };
+	// Parse the dashboard panels
+	var dashboardDir = path.resolve(bundle.dir, 'dashboard');
+	bundle.dashboard = {
+		dir: dashboardDir,
+		panels: parsePanels(dashboardDir, manifest)
+	};
 
-    // Parse the graphics
-    var graphicsDir = path.resolve(bundle.dir, 'graphics');
-    bundle.graphics = parseGraphics(graphicsDir, manifest);
+	// Parse the graphics
+	var graphicsDir = path.resolve(bundle.dir, 'graphics');
+	bundle.graphics = parseGraphics(graphicsDir, manifest);
 
-    // Determine if this bundle has an extension that should be loaded by NodeCG
-    bundle.hasExtension = parseExtension(bundle.dir, manifest);
+	// Determine if this bundle has an extension that should be loaded by NodeCG
+	bundle.hasExtension = parseExtension(bundle.dir, manifest);
 
-    return bundle;
+	return bundle;
 };
